b35b6bebb70ccdf7a1f1dd7ac5b25858a984427d
ROCKET <kr337909@gmail.com>
2026-02-26 01:55:11 -0600

    2123

--- TMessagesProj/src/main/java/org/spacegram/translator/BaseTranslator.java
+++
@@ -1,7 +0,0 @@
-package org.spacegram.translator;
-
-import org.telegram.messenger.Utilities;
-
-public interface BaseTranslator {
-    void translate(String text, String fromLang, String toLang, Utilities.Callback2<String, Boolean> done);
-}
